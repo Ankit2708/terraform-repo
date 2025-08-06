accbceb7548ffa9b4821a74f39b50bd6d7d82db4
Ankit <shivhare.ankit476@gmail.com>
2025-08-06 19:34:12 +0530

    sample terraform file added

--- sample.tf
+++ sample.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 resource "aws_instance" "myterraformtest" {
   ami = "ami-020cba7c55df1f615"
-  instance_type = "t2-micro"
+  instance_type = "t2.micro"
   key_name = "linuxserver1key.pem"
-  security_groups = ["sg-0b26b728e195aa341"]
+  vpc_security_group_ids  = ["sg-0b26b728e195aa341"]
 }
