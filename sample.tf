bba2df6c486e7bfffd84331c0583dfe2bdfb9b6b
Ankit <shivhare.ankit476@gmail.com>
2025-08-07 17:16:52 +0530

    backend code for saving backup file to s3 added

--- sample.tf
+++ sample.tf
@@ -1,9 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket = "terraformtestbucket112"
+    region = "us-east-1"
+  }
+}
 provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "myterraformtest" {
   ami = "ami-020cba7c55df1f615"
   instance_type = "t2.micro"
-  key_name = "linuxserver1key.pem"
+  key_name = "linuxserver1key"
   vpc_security_group_ids  = ["sg-0b26b728e195aa341"]
+  tags = {
+    Name = "instancefrontffile"
+  }
 }
