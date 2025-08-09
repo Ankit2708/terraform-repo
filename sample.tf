e4ef0bbd670a9e2df053c699c646d8afe210d502
Ankit <shivhare.ankit476@gmail.com>
2025-08-09 22:31:47 +0530

    backend code removed

--- sample.tf
+++ sample.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-# terraform {
-#   backend "s3" {
-#     bucket = "terraformtestbucket112"
-#     key = "terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
 provider "aws" {
   region = var.region
 }
