c42ffe40678d934bd3631def39f12a961986d4b1
Ankit <shivhare.ankit476@gmail.com>
2025-08-09 22:17:21 +0530

    security grp code added

--- sample.tf
+++ sample.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 # terraform {
 #   backend "s3" {
 #     bucket = "terraformtestbucket112"
