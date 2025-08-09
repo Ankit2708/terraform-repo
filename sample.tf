f75ffaf328001e9df863911f66abf2ca29db34eb
Ankit <shivhare.ankit476@gmail.com>
2025-08-09 22:40:58 +0530

    backend code removed

--- sample.tf
+++ sample.tf
@@ -1,3 +1,10 @@
+# terraform {
+#   backend "s3" {
+#     bucket = "terraformtestbucket112"
+#     key = "terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
 provider "aws" {
   region = var.region
 }
