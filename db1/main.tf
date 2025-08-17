759f42871270cb876b47cc3be29d46b9be9b8557
Ankit <shivhare.ankit476@gmail.com>
2025-08-17 21:56:51 +0530

    db file added

---
+++ db1/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_db_instance" "mydb" {
+  instance_class = "db.t4g.micro"
+  allocated_storage = 10
+  engine = "mysql"
+  engine_version = "8.0"
+  db_name = mydb123
+  manage_master_user_password = false
+  username = "admin"
+  password = "admin123"
+  vpc_security_group_ids = ["sg-0b26b728e195aa341"]
+  publicly_accessible = false
+  parameter_group_name = "default.mysql8.0"
+}
